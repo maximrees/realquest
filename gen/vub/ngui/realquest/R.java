94f29ba338eb46a7ef3108f6dc595108c7476a34
beOb3r0n@gmail.com <beOb3r0n@gmail.com@ddd75f4d-3f6b-366e-5e8e-331b1b3a4c9f>
2012-11-07 15:21:54 +0000

    Buttons op de main toegevoegd, Activities aangemaakt en aangekoppeld aan de buttons. Activities: Scores, Settings en Credits

--- gen/vub/ngui/realquest/R.java
+++ gen/vub/ngui/realquest/R.java
@@ -20,24 +20,39 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int credits=0x7f080002;
-        public static final int menu_settings=0x7f080004;
-        public static final int options=0x7f080001;
-        public static final int quit=0x7f080003;
+        public static final int credits=0x7f080003;
+        public static final int menu_settings=0x7f080005;
+        public static final int quit=0x7f080004;
+        public static final int scores=0x7f080001;
+        public static final int settings=0x7f080002;
         public static final int startContinue=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_credits=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_scores=0x7f030002;
+        public static final int activity_settings=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_credits=0x7f070000;
+        public static final int activity_main=0x7f070001;
+        public static final int activity_scores=0x7f070002;
+        public static final int activity_settings=0x7f070003;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
-        public static final int button_startcontinue=0x7f050004;
+        public static final int button_credits=0x7f050009;
+        public static final int button_quit=0x7f05000a;
+        public static final int button_scores=0x7f050007;
+        public static final int button_settings=0x7f050008;
+        public static final int button_startcontinue=0x7f050006;
+        public static final int credits=0x7f05000b;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int title_activity_credits=0x7f050004;
         public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_scores=0x7f05000c;
+        public static final int title_activity_settings=0x7f050005;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
