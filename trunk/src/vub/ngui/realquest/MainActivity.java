26b968795accc80e82bb3498fe2620c0194c7705
maximrees <maximrees@ddd75f4d-3f6b-366e-5e8e-331b1b3a4c9f>
2012-11-07 13:16:41 +0000

    none

--- trunk/src/vub/ngui/realquest/MainActivity.java
+++ trunk/src/vub/ngui/realquest/MainActivity.java
@@ -4,19 +4,22 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 	
-	public TextView some;
+	
 
-    @Override
+
+    private Button startContinue;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        some = (TextView) findViewById(R.id.textfield);
-        
+        startContinue = (Button) findViewById(R.id.startContinue);
     }
 
     @Override
