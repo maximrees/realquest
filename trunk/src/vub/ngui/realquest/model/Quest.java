c5d7d4c2213f9752790a6c87932e3afa6753565a
maximrees@gmail.com <maximrees@gmail.com@ddd75f4d-3f6b-366e-5e8e-331b1b3a4c9f>
2012-11-25 21:12:12 +0000

    small pre-mapapplication with multiplechoice minigames as first minigame -update. key required for mapview tile generation.

--- trunk/src/vub/ngui/realquest/model/Quest.java
+++ trunk/src/vub/ngui/realquest/model/Quest.java
@@ -1,13 +1,29 @@
 package vub.ngui.realquest.model;
 
+import java.util.ArrayList;
+
 public class Quest {
 	
 	private String title;
 	private String description;
+	private ArrayList<MiniGame> miniGameInfo = new ArrayList<MiniGame>(); 
 	public Quest(String title, String description) {
 		super();
 		this.title = title;
 		this.description = description;
+	}
+	public Quest(String title, String description,
+			ArrayList<MiniGame> arrayList) {
+		super();
+		this.title = title;
+		this.description = description;
+		this.miniGameInfo = arrayList;
+	}
+	public ArrayList<MiniGame> getMiniGameInfo() {
+		return miniGameInfo;
+	}
+	public void setMiniGameInfo(ArrayList<MiniGame> miniGameInfo) {
+		this.miniGameInfo = miniGameInfo;
 	}
 	public String getTitle() {
 		return title;
